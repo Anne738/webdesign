0f6b5b8a8e831080331b34f7a56980f343fdd19f
Anne738 <94481296+Anne738@users.noreply.github.com>
2022-10-20 21:57:46 +0300

    lab5

--- lab/lab5/exp/public/js/main.js
+++ lab/lab5/exp/public/js/main.js
@@ -1,17 +1,56 @@
-const out = document.getElementById("output");
-let paragraph;
+// const out = document.getElementById("output");
+// let paragraph;
 
-function buttonClick(){
+// function buttonClick(){
+//     fetch("http://localhost:3000/pencils").then(
+//         (response)=>{
+//             return response.text();
+//         }
+//     ).then((text)=>{
+//         let pencilsArray = JSON.parse(text);
+//         pencilsArray.forEach(element => {
+//             paragraph = document.createElement('p');
+//             paragraph.innerText=`id: ${element.id} name: ${element.name} hardness: ${element.hardness} `;
+//             out.appendChild(paragraph);
+//         });
+//     })
+// }
+
+async function addpencils(id, name, hardness, img) {
+    let pen = document.createElement('div')
+    pen.className = "box";
+    let pencilContainer = document.querySelector('.items-container')
+
+    let pencildiv = `
+
+        <h3 class="i-title">${id}. ${name}</h3>
+        <div class="content">
+            <span>Hardness:</span>
+            <p class="i-type">${hardness}</p>
+        </div>
+        <img src="${img}" alt="" class="i-image">`
+    pen.innerHTML = pencildiv;
+    pencilContainer.append(pen);
+}
+
+async function getpencils() 
+{
     fetch("http://localhost:3000/pencils").then(
-        (response)=>{
-            return response.text();
+        (res)=>{
+            return res.text()
         }
     ).then((text)=>{
-        let pencilsArray = JSON.parse(text);
-        pencilsArray.forEach(element => {
-            paragraph = document.createElement('p');
-            paragraph.innerText=`id: ${element.id} name: ${element.name} hardness: ${element.hardness} image_link: ${element.image_link} `;
-            out.appendChild(paragraph);
-        });
+        let teaArray = JSON.parse(text)
+        teaArray.forEach(el => {
+
+            id = `${el.id}`
+            nam =`${el.name}`
+            hardness = `${el.hardness}`
+            img = `${el.image_link}`
+
+            addpencils(id, nam, hardness, img)
+        })
     })
 }
+
+getpencils();
